0e5ef6707ff0e436ff43c1a053c11ae5468bc908
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-01-07 23:38:12 +0000

    Prod env on vagrant SCM polling

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,6 @@
 public class Greetings {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java again1");
+        System.out.println("Hello World from Java from production environment");
+        System.out.println("Using SCM polling on vagrant");
     }
 }
