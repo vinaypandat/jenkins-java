6b2ee4c1743f4b689295d251fcee225f32f5f9ba
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-01-07 22:40:26 +0000

    SCM poll method jenkins test from develop

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,6 @@
 public class Greetings {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java again1");
+        System.out.println("Hello World from Java fro develop branch");
+        System.out.println("Using SCM poll");
     }
 }
