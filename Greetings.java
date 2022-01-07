906f7e631620f69e10a338b05143542f418a2665
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-01-07 23:36:34 +0000

    Test node jenkins SCm polling

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,6 @@
 public class Greetings {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java again1");
+        System.out.println("Hello World from Java from test environment");
+        System.out.println("Using SCM polling in vagrant");
     }
 }
