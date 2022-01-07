9d5ca599da5e294aac95f1a662c1aa0ef1534834
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-01-07 23:10:47 +0000

    Checking github webhooks on AWS

--- Greetings.java
+++ Greetings.java
@@ -1,6 +1,7 @@
 public class Greetings {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java fro develop branch");
-        System.out.println("Using SCM poll");
+        System.out.println("Hello World from Java from develop branch");
+        System.out.println("SCM polling is checked on vagrant");
+        System.out.println("Using Github webhooks on AWS");
     }
 }
