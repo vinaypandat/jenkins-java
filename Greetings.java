4d4fa5f1d16691a0cf7683005c3ae45db4c0cf62
Vinay Pandey <66559525+vinaypandat@users.noreply.github.com>
2022-01-08 17:21:52 +0000

    Update Greetings.java

--- Greetings.java
+++ Greetings.java
@@ -1,6 +1,6 @@
 public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello World from Java from test environment");
-        System.out.println("Using SCM polling in vagrant");
+        System.out.println("webhhok for assignment 2 test job pull from test branch");
     }
 }
