70c50cdac6076459ea4c4772de79ad9eaf0e6367
Vinay Pandey <66559525+vinaypandat@users.noreply.github.com>
2022-01-08 16:49:31 +0000

    Update Greetings.java

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,5 @@
 public class Greetings {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java again1");
+        System.out.println("Hello World from Java for assignment-1 using webhook");
     }
 }
