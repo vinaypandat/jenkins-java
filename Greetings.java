e0d8fcb333a48f3f71c4425417cc149b1eb55e7a
Vinay Pandey <66559525+vinaypandat@users.noreply.github.com>
2022-01-11 18:52:23 +0000

    Update Greetings.java

--- Greetings.java
+++ Greetings.java
@@ -2,6 +2,6 @@ public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello World from Java from develop branch");
         System.out.println("SCM polling is checked on vagrant");
-        System.out.println("Using Github webhooks on AWS");
+        System.out.println("Using Github webhooks on ngrock");
     }
 }
