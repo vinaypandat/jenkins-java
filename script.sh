c07d55bffef3b47e16cad0ee28785024fea4ccd2
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-01-14 18:44:41 +0000

    Added html file for Intellipaat case study

---
+++ script.sh
@@ -0,0 +1,8 @@
+echo "FROM vinaypandat/apache" > Dockerfile
+echo "COPY index.html /var/www/html/" >> Dockerfile
+echo "COPY ./images /var/www/html/" >> Dockerfile
+echo "CMD service apache2 start" >> Dockerfile
+
+sudo docker build -t server_image .
+sudo docker run -t -d -p 82:80 --name apache_server server_image
+sudo docker ps -a
