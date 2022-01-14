329150e0b04f834fc2482203e68009375faf47f5
Vinay Pandey <66559525+vinaypandat@users.noreply.github.com>
2022-01-14 18:04:07 +0000

    Update Greetings.java

--- Greetings.java
+++ Greetings.java
@@ -1,7 +1,7 @@
 public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello World from Java from develop branch");
-        System.out.println("Trigger for assignment 3");
+        System.out.println("Trigger for assignment 3 using webhook");
 //         System.out.println("Using Github webhooks on ngrock");
     }
 }
