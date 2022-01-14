6378ce23e022191a4bf14979e90456bda2ab8f7e
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-01-14 19:27:11 +0000

    update script

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
-echo "FROM vinaypandat/apache" > Dockerfile
+sudo docker rm -f apache_server
+sudo docker rmi server_image
+echo "FROM ubuntu/apache2" > Dockerfile
 echo "COPY index.html /var/www/html/" >> Dockerfile
-echo "COPY ./images /var/www/html/" >> Dockerfile
+echo "COPY ./images /var/www/html/images" >> Dockerfile
 echo "EXPOSE 80" >> Dockerfile
-echo "CMD service apache2 start" >> Dockerfile
 
 sudo docker build -t server_image .
 sudo docker run -t -d -p 82:80 --name apache_server server_image
